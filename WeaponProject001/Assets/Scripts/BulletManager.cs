f16234dad38390f6ff36a03a1a787258fb83c3e0
leejeeyong <fghj0720@gmail.com>
2017-10-21 16:41:25 +0900

    2017.10.21

---
+++ WeaponProject001/Assets/Scripts/BulletManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour {
+     static int enemyHP;
+     static int atk;
+     public int Weaponnum;
+    
+
+	// Use this for initialization
+	void Start () {
+        enemyHP = PlayerPrefs.GetInt("ENEMYHP");
+        if (Weaponnum==1)
+        {
+            atk = PlayerPrefs.GetInt("W1ATK");
+        }
+        else if (Weaponnum == 2)
+        {
+            atk = PlayerPrefs.GetInt("W2ATK");
+        }
+        else if (Weaponnum == 3)
+        {
+            atk = PlayerPrefs.GetInt("W3ATK");
+        }
+        else if (Weaponnum == 4)
+        {
+            atk = PlayerPrefs.GetInt("W4ATK");
+        }
+        Debug.Log("atk" + atk);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(6.0f,0f,0f) , 0.3f);
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        //부딪힌 객체의 태그를 비교해서 적인지 판단합니다.
+        {
+            enemyHP = enemyHP - atk;
+            Destroy(this.gameObject);
+            PlayerPrefs.SetInt("ENEMYHP", enemyHP);
+            //자신을 파괴합니다.
+        }
+    }
+
+}
--- WeaponProject001/Assets/Scripts/MainManager.cs
+++ WeaponProject001/Assets/Scripts/MainManager.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour {
 
+    public BoxCollider2D boxclollider;//클릭영역부분
+    public GameObject Enemy;
+    public GameObject bullet;
+    static GameObject shootBull;
+
     struct Weapons
     {
         static bool ex;
@@ -15,10 +20,10 @@ public class MainManager : MonoBehaviour {
     //기본재화
     static int gold;
     //무기 슬롯 활성 유무
-    static int W1e;
-    static int W2e;
-    static int W3e;
-    static int W4e;
+    static int W1e=0;
+    static int W2e=0;
+    static int W3e=0;
+    static int W4e=0;
     //무기 공격력
     static int W1atk;
     static int W2atk;
@@ -50,36 +55,137 @@ public class MainManager : MonoBehaviour {
     public GameObject W2BTN;
     public GameObject W3BTN;
     public GameObject W4BTN;
-    //무기 슬롯 이미지
-    public GameObject W1SPI;
-    public GameObject W2SPI;
-    public GameObject W3SPI;
-    public GameObject W4SPI;
-    //무기 이미지
-    public Sprite weapon001;
-    public Sprite weapon002;
 
-    public AnimationClip w1ani;
+    //무기 이미지
+    public RuntimeAnimatorController[] weaponClass;
+    //public RuntimeAnimatorController weaponClass002;
+    //public RuntimeAnimatorController weaponClass003;
+    //public RuntimeAnimatorController weaponClass004;
+
+    //무기슬롯
+    public GameObject weapon001;
+    public GameObject weapon002;
+    public GameObject weapon003;
+    public GameObject weapon004;
+
+    public static int EnemyHP;
+
 
 
 
     // Use this for initialization
     void Start () {
+        // PlayerPrefs.SetInt("ENEMYHP", 100);
+        //PlayerPrefs.SetInt("W1ATK", 5);
+        /*
         PlayerPrefs.SetInt("W1E",0);
         PlayerPrefs.SetInt("W2E", 0);
         PlayerPrefs.SetInt("W3E", 0);
         PlayerPrefs.SetInt("W4E", 0);
 
+        PlayerPrefs.SetInt("W1CLASS", 0);
+        PlayerPrefs.SetInt("W2CLASS", 0);
+        PlayerPrefs.SetInt("W3CLASS", 0);
+        PlayerPrefs.SetInt("W4CLASS", 0);
+        */
+
+        //Debug.Log(W1e);
+        //Debug.Log(PlayerPrefs.GetInt("W!E"));
+        W1e = PlayerPrefs.GetInt("W1E");
+       // Debug.Log(W1e);
+       //Debug.Log(PlayerPrefs.GetInt("W!E"));
+
+        W2e = PlayerPrefs.GetInt("W2E");
+        W3e = PlayerPrefs.GetInt("W3E");
+        W4e = PlayerPrefs.GetInt("W4E");
+        W1class = PlayerPrefs.GetInt("W1CLASS");
+        W2class = PlayerPrefs.GetInt("W2CLASS");
+        W3class = PlayerPrefs.GetInt("W3CLASS");
+        W4class = PlayerPrefs.GetInt("W4CLASS");
+        W1atk = PlayerPrefs.GetInt("W1ATK");
+        W2atk = PlayerPrefs.GetInt("W2ATK");
+        W3atk = PlayerPrefs.GetInt("W3ATK");
+        W4atk = PlayerPrefs.GetInt("W4ATK");
+        EnemyHP = PlayerPrefs.GetInt("ENEMYHP");
+        
+        checkWeapon();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        int state=1;
+        /*
+        PlayerPrefs.SetInt("W1E", 0);
+        PlayerPrefs.SetInt("W2E", 0);
+        PlayerPrefs.SetInt("W3E", 0);
+        PlayerPrefs.SetInt("W4E", 0);
 
-    void checkWeapon(int WNUM)
+        PlayerPrefs.SetInt("W1CLASS", 0);
+        PlayerPrefs.SetInt("W2CLASS", 0);
+        PlayerPrefs.SetInt("W3CLASS", 0);
+        PlayerPrefs.SetInt("W4CLASS", 0);
+        */
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D rayhit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+        if (state==0)
+        {
+            weapon001.GetComponent<Animator>().Play("Base Layer.state_idle",1);
+        }
+        else
+        {
+            state = 0;
+        }
+
+        if (rayhit.collider != null)
+        {
+            if (rayhit.collider == boxclollider && Input.GetMouseButtonDown(0))
+            {
+                weapon001.GetComponent<Animator>().Play("Base Layer.state_shoot");
+                state = 1;
+                shootBull = Instantiate(bullet,weapon001.transform.position+new Vector3(0.8f,0.3f,0),bullet.transform.rotation) as GameObject;
+                shootBull.name = bullet.name + "_" + Time.time;
+                Debug.Log("HP"+EnemyHP);
+            }
+        }
+
+        EnemyHP = PlayerPrefs.GetInt("ENEMYHP");
+
+    }
+
+    void checkWeapon()
     {
 
+        if (W1e == 1)
+        {
+            W1BTN.SetActive(false);
+            weapon001.SetActive(true);
+            weapon001.GetComponent<Animator>().runtimeAnimatorController = weaponClass[W1class-1];
+
+        }
+        if (W2e == 1)
+        {
+            W2BTN.SetActive(false);
+            weapon002.SetActive(true);
+            weapon002.GetComponent<Animator>().runtimeAnimatorController = weaponClass[W2class - 1];
+
+        }
+        if (W3e == 1)
+        {
+            W3BTN.SetActive(false);
+            weapon003.SetActive(true);
+            weapon003.GetComponent<Animator>().runtimeAnimatorController = weaponClass[W3class - 1];
+
+        }
+        if (W4e == 1)
+        {
+            W4BTN.SetActive(false);
+            weapon004.SetActive(true);
+            weapon004.GetComponent<Animator>().runtimeAnimatorController = weaponClass[W4class - 1];
+
+        }
+        
     }
 
     public void BuyWeapon(int WNUM)
@@ -87,37 +193,48 @@ public class MainManager : MonoBehaviour {
         if (WNUM == 1)
         {
             
-            //W1BTN.SetActive(false);
             W1e = 1;
+            W1class = 1;
             PlayerPrefs.SetInt("W1E", W1e);
-            W1BTN.GetComponent<Image>().sprite = weapon001;
-            W1BTN.GetComponent<Button>().enabled = false;
-            //W1BTN.GetComponent<Animation>().RemoveClip();
-            
-
-
+            PlayerPrefs.SetInt("W1CLASS", W1class);
+            Debug.Log(W1e);
+            Debug.Log(PlayerPrefs.GetInt("W1E"));
+            W1BTN.SetActive(false);
+            weapon001.SetActive(true);
+            weapon001.GetComponent<Animator>().runtimeAnimatorController = weaponClass[0];
 
 
         }
         else if (WNUM == 2)
         {
-            W2BTN.SetActive(false);
             W2e = 1;
+            W2class = 1;
             PlayerPrefs.SetInt("W2E", W2e);
-            W2SPI.SetActive(true);
+            PlayerPrefs.SetInt("W2CLASS", W2class);
+            W2BTN.SetActive(false);
+            weapon002.SetActive(true);
+            weapon002.GetComponent<Animator>().runtimeAnimatorController = weaponClass[0];
 
         }
         else if (WNUM == 3)
         {
-            W3BTN.SetActive(false);
             W3e = 1;
+            W3class = 1;
             PlayerPrefs.SetInt("W3E", W3e);
+            PlayerPrefs.SetInt("W3CLASS", W3class);
+            W3BTN.SetActive(false);
+            weapon003.SetActive(true);
+            weapon003.GetComponent<Animator>().runtimeAnimatorController = weaponClass[0];
         }
         else if (WNUM == 4)
         {
-            W4BTN.SetActive(false);
             W4e = 1;
+            W4class = 1;
             PlayerPrefs.SetInt("W4E", W4e);
+            PlayerPrefs.SetInt("W4CLASS", W4class);
+            W4BTN.SetActive(false);
+            weapon004.SetActive(true);
+            weapon004.GetComponent<Animator>().runtimeAnimatorController = weaponClass[0];
         }
     }
 
